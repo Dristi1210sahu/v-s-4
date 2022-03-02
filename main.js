be6f8a482793b1c33147e71fa9bdbfcb6b03665e
Dristi1210sahu <89518155+Dristi1210sahu@users.noreply.github.com>
2022-03-02 07:56:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+
+var canvas=document.getElementById("myCanvas");
+
+
+
+ball_x=20;
+ball_y=20;
+
+block_image_width = 5;
+block_image_height = 5;
+
+golf-h_x=850 ;
+golf-h_y=430 ;
+
+function load_img(){
+	// write code to Upload golf image on the canvas
+	new_image();
+}
+
+function new_image()
+{
+	// write code to Upload ball image on canvas
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		ball_y=ball_y-golf-h_image_height;
+            console.log("golf-h_image height = "+ golf-h_image_height);
+            console.log("when  up key is preesed, X = "+ ball_x + " , Y = "+ ball_y);
+            canvas.remove(ball_object);
+            ball_update();
+
+	}
+
+	function down()
+	{
+		ball_y=ball_y-golf-h_image_height;
+		console.log("golf-h_image height = "+ golf-h_image_height);
+		console.log("when  down key is preesed, X = "+ ball_x + " , Y = "+ ball_y);
+		canvas.remove(ball_object);
+		ball_update();
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			ball_x=ball_x-golf-h_image_height;
+        console.log("golf-h_image height = " + golf-h_image_height);
+        console.log("when  left key is preesed, X = "+ ball_x + " , Y = "+ ball_y);
+        canvas.remove(ball_object);
+        ball_update();
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			ball_x=ball_x-golf-h_image_height;
+        console.log("golf-h_image height = " + golf-h_image_height);
+        console.log("when  left key is preesed, X = "+ ball_x + " , Y = "+ ball_y);
+        canvas.remove(ball_object);
+        ball_update();
+		}
+	}
+	
+}
+
